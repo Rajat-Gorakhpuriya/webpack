9c5c2b1bc7ef094e29df2213474e781a31917b86
Rajat Gorakhpuriya <rajat.gorakhpuriya@gmail.com>
2024-10-14 13:01:52 +0530

    adding gonts

---
+++ src/fonts-loader/src/index.js
@@ -0,0 +1,22 @@
+import style from "./index.css"; // style is only name we can use any name.
+import logo from "./assets/webpack-image.png";
+import "./assets/fonts/SixtyfourConvergence-Regular.ttf";
+
+const btn = document.getElementById("button1");
+const logoEl = document.getElementById("logo");
+logoEl.src = logo;
+
+btn.classList.add([style.header]);
+btn.addEventListener("click",()=> {
+    // const el = document.getElementById("header");
+    el.innerHTML = "I update the code";
+
+    const shopplingList = ["Potato", "Tomato", "LadyFinger", "Onion", "Spinach", "chilly"]
+    const ul = document.getElementById("shoppingList");
+
+    shopplingList.forEach(item => {
+        const tempElement = document.createElement("li");
+        tempElement.innerHTML = item;
+        ul.appendChild(tempElement);
+    })
+})
